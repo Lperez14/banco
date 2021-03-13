05759101fa0b3df4f1af942ee2ba515b7e1adac3
Lperez14 <79065428+Lperez14@users.noreply.github.com>
2021-03-13 00:13:25 -0400

    Add files via upload

---
+++ Ejercicio2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Ejercicio 2
+            Nombre: Luis Perez */
+
+            /*2.	Evalué si un número es PAR o IMPAR y muestre en consola el mensaje.*/
+            int num;
+
+
+            Console.Write("-Digite un numero: ");
+            num = int.Parse(Console.ReadLine());
+
+            if ((num % 2) == 0)
+            {
+                Console.Write("El numero introducido es un numero par");
+            }
+            else 
+            {
+                Console.Write("El numero introducido es un numero impar");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
